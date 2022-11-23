7dc9875284eeb03e524f454719e7472b7e3ff0ef
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-23 18:46:35 +0700

    интервал по номеру четверти

---
+++ Задача2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*  Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y) */
+
+int quarter = ReadInt("Введите номер четверти: ");
+if(quarter == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if(quarter == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if(quarter == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if(quarter == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
+else
+{
+    Console.WriteLine("Четвертей всего 4!");
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}

4f3fec65cc270b4dce91486695a65e6595e7e331
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-23 18:44:08 +0700

    Нахождение четверти через цмкл

---
+++ Задача1-2/Program.cs
@@ -0,0 +1,37 @@
+﻿while(true)
+{
+    int x = ReadInt("Введите точку x: ");
+    int y = ReadInt("Введите точку y: ");
+
+    if(x == 0 || y == 0)
+    {
+        Console.WriteLine("x и y не могут быть равны 0");
+        continue;
+    }
+
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("1");
+    }
+    else if(x < 0 && y > 0)
+    {
+        Console.WriteLine("2");
+    }
+    else if(x < 0 && y < 0)
+    {
+        Console.WriteLine("3");
+    }
+    else if(x > 0 && y < 0)
+    {
+        Console.WriteLine("4");
+    }
+    break;
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+

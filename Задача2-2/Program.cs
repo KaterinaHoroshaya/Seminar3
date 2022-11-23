6b6a092375fb3868960b3e67911785da58b6acce
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-23 18:46:55 +0700

    интервал по номеру четверти со свитч кейсом

---
+++ Задача2-2/Program.cs
@@ -0,0 +1,26 @@
+﻿int quarter = ReadInt("Введите номер четверти: ");
+switch (quarter)
+{
+    case 1:
+        Console.WriteLine("x > 0, y > 0");
+        break;
+    case 2:
+        Console.WriteLine("x < 0, y > 0");
+        break;
+    case 3:
+        Console.WriteLine("x < 0, y < 0");
+        break;
+    case 4:
+        Console.WriteLine("x > 0, y < 0");
+        break;
+    default:
+        Console.WriteLine("Четвертей всего 4!");
+        break;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
